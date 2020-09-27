0cc041eb2c628011a21252a809b4e1eada3b12fe
Mike Hitrov <mike.hitrov8892@gmail.com>
2020-09-27 22:38:32 +0300

    Implemented scrolling without js

--- script.js
+++ script.js
@@ -10,29 +10,3 @@ button[0].addEventListener("click", function () {
         content[0].style.display = "block";
     }
 });
-
-let loremArticle = document.getElementById("loremArticle");
-let farAwayArticle = document.getElementById("farAwayArticle");
-let ciceroArticle = document.getElementById("ciceroArticle");
-let wetherArticle = document.getElementById("wetherArticle");
-let pangramArticle = document.getElementById("pangramArticle");
-
-let openLorem = () => {
-    loremArticle.scrollIntoView();
-}
-
-let openFarAway = () => {
-    farAwayArticle.scrollIntoView();
-}
-
-let openCicero = () => {
-    ciceroArticle.scrollIntoView();
-}
-
-let openWether = () => {
-    wetherArticle.scrollIntoView();
-}
-
-let openPangram = () => {
-    pangramArticle.scrollIntoView();
-}

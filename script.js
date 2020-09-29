b0683d60a51c230f990036772f1807bcc013a0be
mikeHitrov <mike.hitrov@gmail.com>
2020-09-29 14:39:12 +0300

    Made changes to the sidebar

--- script.js
+++ script.js
@@ -2,17 +2,22 @@ let openBtn = document.getElementsByClassName("openSideNav")[0];
 let closeBtn = document.getElementsByClassName("closeBtn")[0];
 
 const showNav = () => {
-    document.querySelector(".sideNav").style.width = "300px";
-    document.querySelector('.main-content').style.marginLeft = "300px";
+    document.querySelector(".sideNav").style.width = "20%";
+    document.getElementById('mainContent').style.width = "80%";
+    document.getElementById('navbar').style.width = "80%";
+    document.getElementById('mainContent').style.marginLeft = "20%";
+    document.getElementById('navbar').style.marginLeft = "20%";
 }
 
 const hideNav = () => {
-    document.querySelector(".sideNav").style.width = "0";
-    document.querySelector('.main-content').style.marginLeft = "0px";
+    document.querySelector(".sideNav").style.width = "0%";
+    document.getElementById('mainContent').style.width = "100%";
+    document.getElementById('navbar').style.width = "100%";
+    document.getElementById('mainContent').style.marginLeft = "0%";
+    document.getElementById('navbar').style.marginLeft = "0%";
 }
 
 openBtn.addEventListener("click", () => {
-    console.log(1)
     showNav();
 });
 
